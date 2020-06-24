f4c61964e8271d49f09f7b0bfc9b9ef0a9612510
LemonITCN <liuri@lemonit.cn>
2020-06-24 10:55:47 +0800

    增加日志

--- main.py
+++ main.py
@@ -26,10 +26,12 @@ def read_asin_data(excel_path: str) -> []:
         row_index = row_index + 1
     return name_list
 
-
+print('正在初始化程序数据...')
 global_data_module.init()
+print('正在解析配置文件并读取Excel...')
 # 读取Excel
 file_names = read_asin_data(config_module.get_config_obj()[SOURCE_EXCEL])
+print('正在解码PDF...')
 # 读取PDF
 pdf_doc = fitz.open(config_module.get_config_obj()[SOURCE_PDF])
 print('PDF页数：' + str(pdf_doc.pageCount) + ', EXCEL 记录数：' + str(len(file_names)))
@@ -40,7 +42,7 @@ if not os.path.exists(config_module.get_config_obj()[OUTPUT_DIR]):
     os.makedirs(config_module.get_config_obj()[OUTPUT_DIR])
 for page_index in range(pdf_doc.pageCount):
     pdf_page = pdf_doc[page_index]
-    print('开始转换写出第 ' + str(page_index) + ' / ' + str(len(file_names)) + ' 页图片数据')
+    print('开始转换写出第 ' + str(page_index + 1) + ' / ' + str(len(file_names)) + ' 页图片数据')
     pdf_page.getPixmap(matrix=mat).writePNG(
         config_module.get_config_obj()[OUTPUT_DIR] + os.sep + file_names[page_index] + '.png')
 
